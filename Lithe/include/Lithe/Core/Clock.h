b2d8d6b093def276cf132bfa895d506bdd0d7a75
Vullnet <vuvogli@gmail.com>
2024-12-21 14:54:47 +0100

    Added `Clock` class

---
+++ Lithe/include/Lithe/Core/Clock.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <chrono>
+#include <cassert>
+
+namespace Lithe {
+
+	namespace Time {
+
+		using Timestep = double;
+
+		class Clock {
+			
+			public:
+
+				using ClockType = std::chrono::high_resolution_clock;
+				using TimePoint = ClockType::time_point;
+				using Duration = std::chrono::duration<Timestep>;
+
+				Clock(uint32_t n = 10): mStart(ClockType::now()), mLastUpdate(mStart), mTickInterval(Duration(1.0 / n > 0 ? 1.0 / n : 1.0)) { }
+				
+				// Set the tick interval in seconds
+				void setTickInterval(Timestep interval) {
+					mTickInterval = Duration(interval);
+				}
+
+				// Get the tick interval in seconds
+				Timestep tickInterval() const {
+					return mTickInterval.count();
+				}
+
+				// Get the elapsed time in seconds since the last tick
+				Timestep timeSinceLastUpdate() {
+					auto now = ClockType::now();
+					Duration duration = now - mLastUpdate;
+					mLastUpdate = now;
+					return duration.count();
+				}
+
+				// Get the total elapsed time in seconds since the clock started
+				Timestep elapsed() const {
+					return Duration(ClockType::now() - mStart).count();
+				}
+
+
+			private:
+				
+				TimePoint mStart;
+				TimePoint mLastUpdate;
+				Duration mTickInterval;
+
+		};
+
+	};
+
+}
