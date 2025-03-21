d0c3bbae61fec67b269096abb094cb407a372950
Vullnet <vuvogli@gmail.com>
2025-03-21 16:46:07 +0100

    Update CMakeLists.txt to refine include directories and add private headers

---
+++ Lithe/private/WindowWrapper.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Window.h"
+
+#include <LLGL/LLGL.h>
+#include <LLGL/Surface.h>
+
+namespace Lithe { 
+
+	class WindowWrapper: public LLGL::Surface {
+
+		public:
+
+			WindowWrapper(Lithe::Window* window): pWindow(window) { }
+
+			bool GetNativeHandle(void* nativeHandle, std::size_t nativeHandleSize) final;
+			[[nodiscard]] LLGL::Extent2D GetContentSize() const final;
+			bool AdaptForVideoMode(LLGL::Extent2D* resolution, bool* fullscreen) final;
+			[[nodiscard]] LLGL::Display* FindResidentDisplay() const final;
+
+		private:
+
+			Lithe::Window* pWindow;
+
+	};
+
+}
