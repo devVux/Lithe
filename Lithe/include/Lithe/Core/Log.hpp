afe0246486431b223577c2fb105943347b24abd4
Vullnet <vuvogli@gmail.com>
2026-01-24 14:07:28 +0100

    Updated logging system

--- Lithe/include/Lithe/Core/Log.hpp
+++ Lithe/include/Lithe/Core/Log.hpp
@@ -1,41 +1,44 @@
 #pragma once
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+#include <expected>
+#include <memory>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/rotating_file_sink.h>
-#include <memory>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 #include <string>
 
 namespace Lithe {
 
 class Logger {
 
-	public:
+  public:
 
-		static std::shared_ptr<spdlog::logger>& coreLogger() {
-			static std::shared_ptr<spdlog::logger> logger = initLogger();
-			return logger;
-		}
+	static std::shared_ptr<spdlog::logger>& coreLogger() {
+		static std::shared_ptr<spdlog::logger> logger = initLogger();
+		return logger;
+	}
 
+  private:
 
-	private:
-
-		static std::shared_ptr<spdlog::logger> initLogger(
-			const std::string& name = "Core",
-			const std::string& logFile = "logs/core.log",
-			size_t maxFileSize = 1048576 * 5,
-			size_t maxFiles = 3
-		);
-
+	static std::shared_ptr<spdlog::logger> initLogger(
+		const std::string& name		   = "Core",
+		const std::string& logFile	   = "logs/core.log",
+		size_t			   maxFileSize = 1'048'576 * 5,
+		size_t			   maxFiles	   = 3
+	);
 };
 
-}
+#define LT_LOG_TRACE(...) Logger::coreLogger()->trace(__VA_ARGS__)
+#define LT_LOG_DEBUG(...) Logger::coreLogger()->debug(__VA_ARGS__)
+#define LT_LOG_INFO(...) Logger::coreLogger()->info(__VA_ARGS__)
+#define LT_LOG_WARN(...) Logger::coreLogger()->warn(__VA_ARGS__)
+#define LT_LOG_ERROR(...) Logger::coreLogger()->error(__VA_ARGS__)
+#define LT_LOG_CRITICAL(...) Lithe::Logger::coreLogger()->critical(__VA_ARGS__)
+#define LT_LOG_FATAL(...)                                   \
+	{                                                       \
+		Lithe::Logger::coreLogger()->critical(__VA_ARGS__); \
+		return 1;                                           \
+	}
 
-#define LT_TRACE(...)    Lithe::Logger::coreLogger()->trace(__VA_ARGS__)
-#define LT_DEBUG(...)    Lithe::Logger::coreLogger()->debug(__VA_ARGS__)
-#define LT_INFO(...)     Lithe::Logger::coreLogger()->info(__VA_ARGS__)
-#define LT_WARN(...)     Lithe::Logger::coreLogger()->warn(__VA_ARGS__)
-#define LT_ERROR(...)    Lithe::Logger::coreLogger()->error(__VA_ARGS__)
-#define LT_CRITICAL(...) Lithe::Logger::coreLogger()->critical(__VA_ARGS__)
-
+} // namespace Lithe
