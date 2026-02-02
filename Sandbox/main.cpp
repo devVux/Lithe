3eb91ff8c8049ee8a68b3cb0f0ca2c5fca398a3e
Vullnet <vuvogli@gmail.com>
2026-02-02 10:25:40 +0100

    Cube rendering

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,10 +1,18 @@
-#include "EventDispatcher.hpp"
-#include "Events/WindowEvents.hpp"
-#include "MyWindow.hpp"
-#include "RenderSystem.hpp"
-
-#include <GLFW/glfw3.h>
 #include <Log.hpp>
+#include <EventDispatcher.hpp>
+#include <Events/WindowEvents.hpp>
+#include <RenderSystem.hpp>
+#include <Scene.hpp>
+#include <PerspectiveCamera.hpp>
+#include <Clock.hpp>
+#include <RenderPacket.hpp>
+#include <ResourceCache.hpp>
+#include <GLFW/glfw3.h>
+
+#include "MyWindow.hpp"
+#include "MyInput.hpp"
+
+#include <memory>
 
 using namespace Lithe;
 
@@ -17,6 +25,7 @@ int main() {
 		LT_LOG_CRITICAL("Could not init window");
 		return 1;
 	}
+	MyInput input(w.glfwHandle());
 
 	using Extension = const char*;
 
@@ -35,16 +44,69 @@ int main() {
 		return true;
 	});
 
+
+
+	ResourceCache cache;
+	cache.addMesh({
+		.position = {
+			{-3.0f, -3.0f, -1.0f},
+			{ 3.0f, -3.0f, -1.0f},
+			{ 3.0f,  3.0f, -1.0f},
+			{-3.0f,  3.0f, -1.0f},
+			{-3.0f, -3.0f,  1.0f},
+			{ 3.0f, -3.0f,  1.0f},
+			{ 3.0f,  3.0f,  1.0f},
+			{-3.0f,  3.0f,  1.0f}
+		},
+		.indices = {
+			0,1,2, 2,3,0,
+			4,5,6, 6,7,4,
+			0,4,7, 7,3,0,
+			1,5,6, 6,2,1,
+			3,2,6, 6,7,3,
+			0,1,5, 5,4,0
+		}
+	});
+
 	RenderSystem system;
 	if (not system.init(w, {glfwExtensions.begin(), glfwExtensions.end()})) {
 		LT_LOG_CRITICAL("Could not init renderer");
 		return 1;
 	}
 
+	StaticRenderPacket statics;
+	statics.meshes.push_back(0);
+	system.uploadStaticData(statics, cache);
+
+	DynamicRenderPacket dynamics;
+	dynamics.camera = glm::mat4(1.0f);
+
+	Scene scene;
+	scene.setCamera(
+		std::make_unique<PerspectiveCamera>(
+			glm::vec3(0.0f, 0.0f, 2.0f),
+			glm::vec3(0.0f, 0.0f, -1.0f)
+		)
+	);
+
+
+
+	Clock clock(60);
+	double acc = 0.0;
+
 	while (running) {
-		system.render();
+		acc += clock.timeSinceLastUpdate();
+
+		while (acc >= clock.tickInterval()) {
+			scene.update(clock.tickInterval(), input);
+			acc -= clock.tickInterval();
+		}
+
+		dynamics.camera = scene.camera();
+		system.render(dynamics, cache);
 		w.update(dispatcher);
 	}
+
 
 	LT_LOG_TRACE("Bye world");
 }
