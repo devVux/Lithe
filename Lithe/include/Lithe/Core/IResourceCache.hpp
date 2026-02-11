df38d493b7b30ba894c36b203f9770d0af5d15a3
Vullnet <vuvogli@gmail.com>
2026-02-11 11:25:47 +0100

    Implemented resource cache

--- Lithe/include/Lithe/Core/IResourceCache.hpp
+++ Lithe/include/Lithe/Core/IResourceCache.hpp
@@ -6,36 +6,38 @@
 
 namespace Lithe {
 
-class IResourceCache {
-
-	public:
-		
-		virtual ~IResourceCache() = default;
-
-		[[nodiscard]] virtual MeshData getMesh(MeshID) const noexcept = 0;
-		[[nodiscard]] virtual std::optional<MeshID> addMesh(MeshData data) noexcept = 0;
-
-		[[nodiscard]] virtual MaterialData getMaterial(MaterialID) const noexcept = 0;
-		[[nodiscard]] virtual std::optional<MaterialID> addMaterial(MaterialData) noexcept = 0;
-
-		[[nodiscard]] virtual TextureData getTexture(TextureID) const noexcept = 0;
-		[[nodiscard]] virtual std::optional<TextureID> addTexture(TextureData) noexcept = 0;
-
-		[[nodiscard]] virtual std::size_t vertexCount() const noexcept = 0;
-		[[nodiscard]] virtual std::size_t indexCount() const noexcept = 0;
-		[[nodiscard]] virtual std::size_t materialCount() const noexcept = 0;
-		[[nodiscard]] virtual std::size_t textureCount() const noexcept = 0;
-
-		[[nodiscard]] virtual Size largestTexture() const noexcept = 0;
-
-
-		// TODO: revisit this
-		// We could do many things as returning IDs and querying the cache directly or
-		// use the visitor pattern for more complex iteration logic
-		[[nodiscard]] virtual std::vector<std::pair<MeshID, MeshData>> meshes() const noexcept = 0;
-		[[nodiscard]] virtual std::vector<std::pair<MaterialID, MaterialData>> materials() const noexcept = 0;
-		[[nodiscard]] virtual std::vector<std::pair<TextureID, TextureData>> textures() const noexcept = 0;
-
+struct Size2 {
+	uint32_t width, height;
 };
 
-}
+class IResourceCache {
+
+public:
+
+	virtual ~IResourceCache() = default;
+
+	[[nodiscard]] virtual MeshData				getMesh(MeshID) const noexcept	= 0;
+	[[nodiscard]] virtual std::optional<MeshID> addMesh(MeshData data) noexcept = 0;
+
+	[[nodiscard]] virtual MaterialData				getMaterial(MaterialID) const noexcept = 0;
+	[[nodiscard]] virtual std::optional<MaterialID> addMaterial(MaterialData) noexcept	   = 0;
+
+	[[nodiscard]] virtual TextureData			   getTexture(TextureID) const noexcept = 0;
+	[[nodiscard]] virtual std::optional<TextureID> addTexture(TextureData) noexcept		= 0;
+
+	[[nodiscard]] virtual std::size_t vertexCount() const noexcept	 = 0;
+	[[nodiscard]] virtual std::size_t indexCount() const noexcept	 = 0;
+	[[nodiscard]] virtual std::size_t materialCount() const noexcept = 0;
+	[[nodiscard]] virtual std::size_t textureCount() const noexcept	 = 0;
+
+	[[nodiscard]] virtual Size2 largestTexture() const noexcept = 0;
+
+	// TODO: revisit this
+	// We could do many things as returning IDs and querying the cache directly or
+	// use the visitor pattern for more complex iteration logic
+	[[nodiscard]] virtual std::vector<std::pair<MeshID, MeshData>>		   meshes() const noexcept	  = 0;
+	[[nodiscard]] virtual std::vector<std::pair<MaterialID, MaterialData>> materials() const noexcept = 0;
+	[[nodiscard]] virtual std::vector<std::pair<TextureID, TextureData>>   textures() const noexcept  = 0;
+};
+
+} // namespace Lithe
--- Lithe/include/Lithe/Scene/ResourceCache.hpp
+++ Lithe/include/Lithe/Scene/ResourceCache.hpp
@@ -6,84 +6,87 @@ namespace Lithe {
 
 class ResourceCache: public IResourceCache {
 
-	public:
+public:
 
-		[[nodiscard]] MeshData getMesh(MeshID id) const noexcept { 
-			return mMeshes[static_cast<std::size_t>(id)];
-		}
+	[[nodiscard]] MeshData getMesh(MeshID id) const noexcept override { return mMeshes[static_cast<std::size_t>(id)]; }
 
-		[[nodiscard]] std::optional<MeshID> addMesh(MeshData data) noexcept {
-			mVertexCount += data.positions.size();
-			mIndexCount += data.indices.size();
-			mMeshes.emplace_back(std::move(data));
+	[[nodiscard]] std::optional<MeshID> addMesh(MeshData data) noexcept override {
+		mVertexCount += data.positions.size();
+		mIndexCount	 += data.indices.size();
+		mMeshes.emplace_back(std::move(data));
 
-			return static_cast<MeshID>(mMeshes.size() - 1);
-		}
+		return static_cast<MeshID>(mMeshes.size() - 1);
+	}
 
-		[[nodiscard]] MaterialData getMaterial(MaterialID id) const noexcept {
-			return mMaterials[static_cast<std::size_t>(id)];
-		}
-		[[nodiscard]] std::optional<MaterialID> addMaterial(MaterialData data) noexcept {
-			mMaterials.emplace_back(std::move(data));
+	[[nodiscard]] MaterialData getMaterial(MaterialID id) const noexcept override {
+		return mMaterials[static_cast<std::size_t>(id)];
+	}
 
-			return static_cast<MaterialID>(mMaterials.size() - 1);
-		}
+	[[nodiscard]] std::optional<MaterialID> addMaterial(MaterialData data) noexcept override {
+		mMaterials.emplace_back(data);
 
-		[[nodiscard]] TextureData getTexture(TextureID id) const noexcept {
-			return mTextures[static_cast<std::size_t>(id)];
-		}
-		[[nodiscard]] std::optional<TextureID> addTexture(TextureData data) noexcept {
-			if (data.width * data.height > mLargestTexture.width * mLargestTexture.height)
-				mLargestTexture = { data.width, data.height };
-				
-			mTextures.emplace_back(std::move(data));
+		return static_cast<MaterialID>(mMaterials.size() - 1);
+	}
 
-			return static_cast<TextureID>(mTextures.size() - 1);
-		}
-		
+	[[nodiscard]] TextureData getTexture(TextureID id) const noexcept override {
+		return mTextures[static_cast<std::size_t>(id)];
+	}
 
-		[[nodiscard]] std::size_t vertexCount() const noexcept override { return mVertexCount; }
-		[[nodiscard]] std::size_t indexCount() const noexcept override { return mIndexCount; }
-		[[nodiscard]] std::size_t materialCount() const noexcept override { return mMaterials.size(); }
-		[[nodiscard]] std::size_t textureCount() const noexcept override { return mTextures.size(); }
+	[[nodiscard]] std::optional<TextureID> addTexture(TextureData data) noexcept override {
+		if (data.width * data.height > mLargestTexture.width * mLargestTexture.height)
+			mLargestTexture = {data.width, data.height};
 
-		[[nodiscard]] Size largestTexture() const noexcept override { return mLargestTexture; }
+		mTextures.emplace_back(std::move(data));
 
-		[[nodiscard]] std::vector<std::pair<MeshID, MeshData>> meshes() const noexcept override {
-			std::vector<std::pair<MeshID, MeshData>> result;
-			for (std::size_t i = 0; i < mMeshes.size(); ++i)
-				result.emplace_back(std::make_pair(static_cast<MeshID>(i), mMeshes[i]));
-				
-			return result;
-		}
+		return static_cast<TextureID>(mTextures.size() - 1);
+	}
 
-		[[nodiscard]] std::vector<std::pair<MaterialID, MaterialData>> materials() const noexcept override {
-			std::vector<std::pair<MaterialID, MaterialData>> result;
-			for (std::size_t i = 0; i < mMaterials.size(); ++i)
-				result.emplace_back(std::make_pair(static_cast<MaterialID>(i), mMaterials[i]));
-				
-			return result;
-		}
+	[[nodiscard]] std::size_t vertexCount() const noexcept override { return mVertexCount; }
 
-		[[nodiscard]] std::vector<std::pair<TextureID, TextureData>> textures() const noexcept override {
-			std::vector<std::pair<TextureID, TextureData>> result;
-			for (std::size_t i = 0; i < mTextures.size(); ++i)
-				result.emplace_back(std::make_pair(static_cast<TextureID>(i), mTextures[i]));
+	[[nodiscard]] std::size_t indexCount() const noexcept override { return mIndexCount; }
 
-			return result;
-		}
+	[[nodiscard]] std::size_t materialCount() const noexcept override { return mMaterials.size(); }
 
+	[[nodiscard]] std::size_t textureCount() const noexcept override { return mTextures.size(); }
 
-	private:
+	[[nodiscard]] Size2 largestTexture() const noexcept override { return mLargestTexture; }
 
-		std::size_t mVertexCount = 0;
-		std::size_t mIndexCount = 0;
-		Size mLargestTexture { 0, 0 };
+	[[nodiscard]] std::vector<std::pair<MeshID, MeshData>> meshes() const noexcept override {
+		std::vector<std::pair<MeshID, MeshData>> result;
+		result.reserve(mMeshes.size());
+		for (std::size_t i = 0; i < mMeshes.size(); ++i)
+			result.emplace_back(std::make_pair(static_cast<MeshID>(i), mMeshes[i]));
 
-		std::vector<MeshData> mMeshes;
-		std::vector<MaterialData> mMaterials;
-		std::vector<TextureData> mTextures;
+		return result;
+	}
 
+	[[nodiscard]] std::vector<std::pair<MaterialID, MaterialData>> materials() const noexcept override {
+		std::vector<std::pair<MaterialID, MaterialData>> result;
+		result.reserve(mMaterials.size());
+		for (std::size_t i = 0; i < mMaterials.size(); ++i)
+			result.emplace_back(std::make_pair(static_cast<MaterialID>(i), mMaterials[i]));
+
+		return result;
+	}
+
+	[[nodiscard]] std::vector<std::pair<TextureID, TextureData>> textures() const noexcept override {
+		std::vector<std::pair<TextureID, TextureData>> result;
+		result.reserve(mTextures.size());
+		for (std::size_t i = 0; i < mTextures.size(); ++i)
+			result.emplace_back(std::make_pair(static_cast<TextureID>(i), mTextures[i]));
+
+		return result;
+	}
+
+private:
+
+	std::size_t mVertexCount = 0;
+	std::size_t mIndexCount	 = 0;
+	Size2		mLargestTexture {.width=0, .height=0};
+
+	std::vector<MeshData>	  mMeshes;
+	std::vector<MaterialData> mMaterials;
+	std::vector<TextureData>  mTextures;
 };
 
-}
+} // namespace Lithe
